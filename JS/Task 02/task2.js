109d99f31a6e65f735f38a551d99d166eea5e465
jydv402 <jydv402@gmail.com>
2023-11-14 00:14:22 +0530

    Task 2 Completed

---
+++ JS/Task 02/task2.js
@@ -0,0 +1,8 @@
+function Sum() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    var sum = num1 + num2;
+    var resultElement = document.getElementById("sum");
+    resultElement.innerText = "The sum is: " + sum;
+}
